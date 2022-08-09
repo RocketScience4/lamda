f06d0fdfa1d16c2b6841965693811209985e4c87
RocketScience4 <103602405+RocketScience4@users.noreply.github.com>
2022-08-09 17:03:14 +0500

    updated

--- src/lamda/labrat1.java
+++ src/lamda/labrat1.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class labrat1 {
+public class labrat2 {
 
 	/**
 	 * @param args
@@ -23,7 +23,7 @@ public class labrat1 {
         Integer page=0;
         driver.get("https://www.etsy.com/?ref=lgo");
         Thread.sleep(10000);
-        driver.findElement(By.id("global-enhancements-search-query")).sendKeys("jewellery");
+        driver.findElement(By.id("global-enhancements-search-query")).sendKeys("cloth");
         driver.findElement(By.xpath("//button[@value='Search']")).click();
         List<WebElement> listImages=driver.findElements(By.tagName("img"));
         for(WebElement image:listImages)
@@ -36,7 +36,7 @@ public class labrat1 {
         System.out.println("No. of total Displayed images on Page 1: "+counter);
         Thread.sleep(10000);
 
-        while (page < 5)
+        while (page < 4)
         {
             driver.findElement(By.xpath("//body/main[@id='content']/div[1]/div[1]/div[1]/div[4]/div[5]/div[3]/div[13]/div[1]/div[1]/div[1]/div[2]/nav[1]/ul[1]/li[11]/a[1]")).click();
             Thread.sleep(5000);
@@ -49,13 +49,12 @@ public class labrat1 {
                     counter++;
                 }
             }
-            System.out.println("No. of total Displayed images on Page : "+counter);
+            System.out.println("No. of total Displayed images on Page "+(page+2)+": "+counter);
             page++;
 
         }
-        
+        System.out.println("Test is completed.");
         driver.quit();
-        
 
     }
 
